73ac4ec8154cdee248991ca8d1c9add84afa0f64
Dominik <dominik.work.dev@gmail.com>
2026-07-16 13:33:07 +0200

    feat: rebuilding main messageCreate func and new trigger model

---
+++ src/index.js
@@ -0,0 +1,134 @@
+require("dotenv").config();
+const triggers = require("./triggers.js");
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  REST,
+  Routes,
+} = require("discord.js");
+
+const dsc_token = process.env.DISCORD_TOKEN;
+const dsc_app_id = process.env.DISCORD_APP_ID;
+
+const rest = new REST({ version: "10" }).setToken(dsc_token);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+async function registerCommands() {
+  try {
+    await rest.put(Routes.applicationCommands(dsc_app_id), {
+      body: [
+        {
+          name: "ping",
+          description: "Sprawdzenie czy bot działa.",
+        },
+      ],
+    });
+    console.log("Komendy zostały zarejestrowane.");
+  } catch (error) {
+    console.error("Błąd podczas rejestracji komend:", error);
+  }
+}
+
+registerCommands();
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.on("clientReady", () => {
+  console.log(`Bot zalogowany jako ${client.user.tag}`);
+});
+
+client.on("guildMemberRemove", (member) => {
+  const channel = member.guild.channels.cache.find((ch) => ch.name === "logi");
+
+  if (channel) {
+    const name =
+      member.displayName !== member.user.username
+        ? `${member.user.username} (${member.displayName})`
+        : member.user.username;
+    channel.send(`Użytkownik **${name}** opuścił serwer.`);
+  }
+});
+
+function pickWeighted(responses) {
+  const totalWeight = responses.reduce(
+    (sum, response) => sum + response.weight,
+    0,
+  );
+  let randomValue = Math.random() * totalWeight;
+  for (const response of responses) {
+    randomValue -= response.weight;
+    if (randomValue <= 0) return response;
+  }
+
+  return responses[0]; // fallback in case of rounding errors
+}
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  const text = message.content.toLowerCase().trim();
+
+  for (const trigger of triggers) {
+    if (!text.includes(trigger.match)) continue; // Check if the message contains the trigger match
+    if (Math.random() > (trigger.chance ?? 1)) continue; // Check if the random chance allows for this trigger to activate
+
+    let response = {
+      text: trigger.text ?? "",
+      customEmoji: trigger.customEmoji ?? "",
+      react: trigger.react ?? "",
+    };
+
+    // 1) Weigthed random response
+    if (trigger.weighted) {
+      const picked = pickWeighted(trigger.responses);
+      response = { ...response, ...picked };
+    }
+
+    // 2) Sending a message + optional custom emoji
+    if (response.text || response.customEmoji) {
+      let finalMessage = response.text;
+      if (response.customEmoji) {
+        const emoji = message.guild.emojis.cache.find(
+          (e) => e.name === response.customEmoji,
+        );
+        const emojiStr = emoji ? emoji.toString() : response.customEmoji;
+
+        finalMessage += response.text ? ` ${emojiStr}` : emojiStr;
+      }
+
+      await message.channel.send(finalMessage);
+    }
+
+    // 3) Reacting to the message with an emoji
+    if (response.react) {
+      const emoji = message.guild.emojis.cache.find(
+        (e) => e.name === response.react,
+      );
+      try{ 
+        await message.react(emoji ?? response.react);
+      }
+      catch (error) {
+        console.error(`Nie udało się zareagować na wiadomość: ${error}`);
+      }
+    }
+
+    break;
+  }
+});
+
+client.login(dsc_token);
---
+++ src/triggers.js
@@ -0,0 +1,122 @@
+/* Types :
+- "message": Sends a simple message
+- "message_with_emoji": Sends a message with an emoji
+- "react": Reacts to the message with an emoji
+- "weighted_random": Sends a random message from a list of responses with weights
+
+
+{
+  match: "",
+  text: "",
+  customEmoji: "", //optional, will be added to text
+  react: "", //optional
+  chance: 1, //optional, default is 1 (100%)
+},
+
+
+If using react and default emoji - dont use :emoji: - use unicode or emoji itself
+
+*/
+
+module.exports = [
+  {
+    match: "??",
+    text: ":question:",
+  },
+  {
+    match: "liga",
+    customEmoji: "pepeGun",
+  },
+  {
+    match: "banana",
+    text: ":banana:",
+  },
+  {
+    match: "bedge",
+    react: "bedge",
+  },
+  {
+    match: "klank",
+    weighted: true,
+    responses: [
+      { customEmoji: ":robot:", weight: 65 },
+      { text: "Klank Klank", weight: 20 },
+      {
+        text: "https://klipy.com/gifs/terminator-terminator-robot-7",
+        weight: 10,
+      },
+      { text: "KLANKER", weight: 5 },
+    ],
+  },
+  {
+    match: "clank",
+    weighted: true,
+    responses: [
+      { customEmoji: ":robot:", weight: 65 },
+      { text: "Clank Clank", weight: 20 },
+      {
+        text: "https://klipy.com/gifs/terminator-terminator-robot-7",
+        weight: 10,
+      },
+      { text: "U called?", weight: 5 },
+    ],
+  },
+  {
+    match: "cringe",
+    text: "https://klipy.com/gifs/cringe-diesfromcringe-1",
+  },
+  {
+    match: "crindzfist",
+    text: ":boxing_glove:",
+  },
+  {
+    match: "doomfist",
+    text: ":boxing_glove:",
+  },
+  {
+    match: "dumfist",
+    text: ":boxing_glove:",
+  },
+  {
+    match: "heartstone",
+    customEmoji: "peepoYellowCard",
+  },
+  {
+    match: "liga",
+    customEmoji: "monkikick",
+  },
+  {
+    match: "genji",
+    customEmoji: "PeepoStop",
+  },
+  {
+    match: "monkey",
+    text: ":monkey_face:",
+  },
+  {
+    match: "monki",
+    react: "🐵",
+  },
+  {
+    match: "pepega",
+    weighted: true,
+    responses: [
+      { text: "pepega", weight: 25 },
+      { customEmoji: "pepega", weight: 15 },
+      { react: "pepega", weight: 15 },
+      { customEmoji: "okayge", weight: 15 },
+      { react: "okayge", weight: 15 },
+      { text: "PEPEGA!", weight: 5 },
+      { text: "https://klipy.com/gifs/pepega-clap", weight: 5 },
+      { text: "U called?", weight: 5 },
+    ],
+  },
+  {
+    match: "🐵",
+    text: ":monkey:",
+  },
+  {
+    match: "🐒",
+    text: ":monkey_face:",
+  },
+];
